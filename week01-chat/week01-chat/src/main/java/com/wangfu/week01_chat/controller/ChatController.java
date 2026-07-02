8160e5765dd604a72d1cb3cc1a005f742b44aa1a
adrian <adrianjiao>
2026-07-02 11:56:13 +0800

    "week01: init spring ai chat demo"

---
+++ week01-chat/week01-chat/src/main/java/com/wangfu/week01_chat/controller/ChatController.java
@@ -0,0 +1,21 @@
+package com.wangfu.week01_chat.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChatController {
+
+    private final ChatClient chatClient;
+
+    public ChatController (ChatClient.Builder builder) {
+        this.chatClient = builder.build();
+    }
+
+    @GetMapping("/chat")
+    public String chat(@RequestParam String message) {
+        return chatClient.prompt().call().content();
+    }
+}
---
+++ week01-chat/week01-chat/src/test/java/com/wangfu/week01_chat/Week01ChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wangfu.week01_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week01ChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
