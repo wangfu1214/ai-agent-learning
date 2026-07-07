fad48814767ae0f9718d34b80fe387d4b2d12a91
adrian <adrianjiao>
2026-07-07 10:00:13 +0800

    feat(starter): add agentflow auto configuration

---
+++ agentflow-spring-boot-starter/src/main/java/com/wangfu/agentflow/autoconfigure/AgentFlowAutoConfiguration.java
@@ -0,0 +1,9 @@
+package com.wangfu.agentflow.autoconfigure;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.wangfu.agentflow.ai")
+public class AgentFlowAutoConfiguration {
+}
